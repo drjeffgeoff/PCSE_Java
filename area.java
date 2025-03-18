83a723bfd7bf1507a0b5ddcb29a7dbef6ac1337a
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-18 17:39:57 +0300

    Added

---
+++ area.java
@@ -0,0 +1,10 @@
+// Calc area of Rectangle 
+public class area {
+    public static void main(String[] args) {
+        int length = 8;
+        int width = 5;
+        int area;
+        area = length * width;
+        System.out.println("Area of a rectangle: " + area);
+    }
+}
