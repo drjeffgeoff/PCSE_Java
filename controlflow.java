9553f176901a11412686c18108cbe15a0bf93e29
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-18 17:40:39 +0300

    Control flow

---
+++ controlflow.java
@@ -0,0 +1,16 @@
+public class controlflow {
+    public static void main(String[] args) {
+        // Conditions age <=13 , Your are a child, age <18 , teenage >18 , Adult
+        int age = 11; // Positive value
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else if (age > 13) {
+            System.out.println("Teenager");
+        } else {
+            System.out.println("Child");
+        
+    }
+
+}
+}
+
