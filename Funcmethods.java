f8ff206aac17401733fe0108f9091bc4b9296942
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-24 10:52:15 +0300

    Added method calliong

---
+++ Funcmethods.java
@@ -0,0 +1,14 @@
+//Functions (also called methods in Java) are blocks of 
+//code that perform a specific task. 
+
+//Define and call a method
+public class Funcmethods {
+    static void greet() {
+        System.out.println("Good morning");
+    }
+
+    // Calling method
+    public static void main(String[] args) {
+        greet(); // Calling the methods
+    }
+}
