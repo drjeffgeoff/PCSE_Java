ebbfbbdc5435c1668ee7327f4e951eb5e17c16e7
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-24 10:51:27 +0300

    Added methods

---
+++ fparameter.java
@@ -0,0 +1,13 @@
+public class fparameter {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(15, 3); // Calling the method with parameters
+        System.out.println("Result: " + result);
+    }
+}
+
+// Calling Methods with Parameters and Return Values for method name to
+// calculate area of rectangle
