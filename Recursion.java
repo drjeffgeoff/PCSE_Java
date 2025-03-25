8fd5e50426ac8c51430e4d93ddc71bd287890bd5
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-25 10:10:56 +0300

    Added recursion in Java

--- Recursion.java
+++ Recursion.java
@@ -1,7 +1,20 @@
 /*
- * 
+ * Recursion is a process where a method calls itself to solve a problem. 
+ * It is useful for solving problems that can be broken down into smaller, 
+ * similar subproblems.
  */
 
 public class Recursion {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1; // Base case
+        } else {
+            return n * factorial(n - 1); // Recursive call
+        }
+    }
 
+    public static void main(String[] args) {
+        int result = factorial(5); // Calling the recursive method
+        System.out.println("Factorial of n number: " + result);
+    }
 }
