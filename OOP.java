f21b389ee6fe8a3479c8033a7cc0926a16f49985
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-25 10:31:23 +0300

    Added OOP concepts

--- OOP.java
+++ OOP.java
@@ -15,6 +15,44 @@ Constructors and Their Types
 Access Modifiers (public, private, protected, default)
 
 The this Keyword
+
+
+Class
+A class is a blueprint or template for creating objects.
+
+It defines attributes (fields) and behaviors (methods) 
+that the objects will have.
+
+Object
+An object is an instance of a class.
+
+Objects have state (fields) and behavior (methods).
+
+Methods
+Methods define the behavior of an object.
+
+They can take parameters and return values.
+
+What is a Constructor?
+A constructor is a special method used to initialize objects.
+
+It has the same name as the class and no return type.
+
+The this Keyword
+5.1 Uses of this
+Referencing the current object
+
+
+Read this
+Class: Blueprint for objects.
+
+Object: Instance of a class.
+
+Constructors: Initialize objects (default, parameterized, overloading).
+
+Access Modifiers: Control visibility (public, private, protected, default).
+
+this Keyword: Refers to the current object.
  */
 public class OOP {
 
