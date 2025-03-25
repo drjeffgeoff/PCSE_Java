bc949e38c600863c709291f10f99291798156703
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-25 10:21:43 +0300

    Added

---
+++ OOP.java
@@ -0,0 +1,21 @@
+/*
+ * Module 5: Object-Oriented Programming (OOP) – Part 1
+ * Object-Oriented Programming (OOP) is a programming paradigm that
+ *  organizes software design around objects rather than functions and logic. 
+ * Java is an OOP language, and understanding its core concepts is essential 
+ * for writing efficient and reusable code.
+ * 
+ * Key OOP Concepts Covered in This Module:
+Classes, Objects, and Methods
+
+Creating Classes and Objects in Java
+
+Constructors and Their Types
+
+Access Modifiers (public, private, protected, default)
+
+The this Keyword
+ */
+public class OOP {
+
+}
