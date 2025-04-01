e5004b443a6c5ef4a1f0796be7259367ee0505e2
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-04-01 10:22:12 +0300

    Added

--- accessModifiers.java
+++ accessModifiers.java
@@ -1,3 +1,26 @@
+//Access modifiers (public, private, protected, default) 
+
+/*
+ *  Key Concepts
+Modifier	Access Scope
+public	Any class
+private	Only within the same class
+protected	Same class + subclasses + same package
+default	Same package only
+ */
+
+/*
+ * The this keyword
+  Key Concepts
+Refers to the current object.
+
+Used to:
+
+Resolve naming conflicts (e.g., this.name = name).
+
+Call one constructor from another (this()).
+ */
+
 public class accessModifiers {
 
 }
