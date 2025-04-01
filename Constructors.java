48fd12975cc25f8e545885b03b2121d38c0e1bf5
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-04-01 10:04:09 +0300

    Added

--- Constructors.java
+++ Constructors.java
@@ -1,3 +1,41 @@
+//Constructors and Their Types
+/*
+ * Constructor: Special method that initializes objects.
+
+Types:
+
+Default Constructor (No arguments)
+
+Parameterized Constructor (Takes arguments)
+ */
+
+//Example One
+class Smartphone {
+    String model;
+    int storageGB;
+
+    // Default constructor
+    Smartphone() {
+        model = "Unknown";
+        storageGB = 128;
+        System.out.println("Default smarthphone created");
+    }
+
+    // Parameterized constructor
+    Smartphone(String m, int storage) {
+        model = m;
+        storageGB = storage;
+        System.out.println(" smarth phone with " + storageGB + "GB");
+    }
+
+}
+
 public class Constructors {
-    
+    public static void main(String[] args) {
+        // For default
+        Smartphone phone1 = new Smartphone();
+
+        // For parameterized
+        Smartphone phone2 = new Smartphone("Iphone 15", 256);
+    }
 }
